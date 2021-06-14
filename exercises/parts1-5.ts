394b9ffed8fa5f5fc70b408bedbc7eba6222dea2
Melinda <codegirl78@gmail.com>
2021-06-14 18:10:51 -0500

    Added code for part 2 and console.log statement for days to Mars

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -10,11 +10,12 @@ let kilometersToTheMoon: number = 384400;
 let milesPerKilometer: number = 0.621;
 
 // Part 2: Print Days to Mars
-
-
+let milesToMars: number = kilometersToMars * milesPerKilometer;
+let hoursToMars: number = milesToMars / speedMph;
+let daysToMars: number = hoursToMars / 24;
 
 // Code an output statement here (use a template literal):
-
+console.log(`${spacecraftName} would take ${daysToMars} to get to Mars`);
 
 
 // Part 3: Create a Function ("getDaysToLocation")
