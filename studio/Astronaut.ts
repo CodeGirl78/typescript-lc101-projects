f33db2fb2e0657e395a890c3009d885f0e71737f
Melinda <codegirl78@gmail.com>
2021-06-14 20:28:22 -0500

    Added code for Astronaut.ts

--- studio/Astronaut.ts
+++ studio/Astronaut.ts
@@ -2,4 +2,10 @@ import { Payload } from './Payload';
 
 export class Astronaut {
     // properties and methods
+    massKg: number;
+    material: string;
+    constructor (massKg: number, material: string) {
+        this.massKg = massKg;
+        this.material = material;
+    }
  }
